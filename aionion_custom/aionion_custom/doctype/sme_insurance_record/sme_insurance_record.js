2ab4b0833cea0671bf96ae5fe0906db6ec85f6cb
Rajagopalan <rajagopalan.s@aionioncapital.com>
2026-05-20 11:25:45 +0000

    feat: SME Insurance Record — full flow with child tables, CRM button, backend functions, permissions

--- aionion_custom/aionion_custom/doctype/sme_insurance_record/sme_insurance_record.js
+++ aionion_custom/aionion_custom/doctype/sme_insurance_record/sme_insurance_record.js
@@ -1,8 +1,5 @@
-// Copyright (c) 2026, developers@buildwithhussain.com and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("SME Insurance Record", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('SME Insurance Record', {
+    refresh(frm) {
+        // SME Insurance Record form loaded
+    }
+});
