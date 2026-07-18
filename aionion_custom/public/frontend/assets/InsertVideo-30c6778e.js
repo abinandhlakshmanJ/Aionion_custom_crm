ab1b842c2150f08ed23e87d63d4e4504e3bb0f04
Abinandh Lakshman <abinandhlakshman20@gmail.com>
2026-07-18 13:31:27 +0000

    Fix Tailwind setup and remove Deals/Organizations/Contacts from sidebar

---
+++ aionion_custom/public/frontend/assets/InsertVideo-30c6778e.js
@@ -0,0 +1,2 @@
+import{l as s,y as t,ax as a,ay as d}from"./index-dc9eb387.js";const p=s({__name:"InsertVideo",props:{editor:{}},setup(e){const o=e;function n(){o.editor.chain().focus().selectAndUploadVideo().run()}return(r,i)=>t(r.$slots,"default",a(d({onClick:n})))}});export{p as default};
+//# sourceMappingURL=InsertVideo-30c6778e.js.map
